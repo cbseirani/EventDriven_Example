c11ab9e12dbb19a3b3a186f8cffd5104e1467c71
Christopher Bseirani <34148393+cbseirani@users.noreply.github.com>
2024-07-15 12:48:51 -0400

    Update ServicesExtensions.cs

--- MainApi/Extensions/ServicesExtensions.cs
+++ MainApi/Extensions/ServicesExtensions.cs
@@ -31,25 +31,5 @@ public static class ServicesExtensions
     
     public static void ConfigureSqlDatabase(this IServiceCollection services, IConfiguration configuration)
     {
-        var dbConnString = configuration.GetSection("DBCONNSTRING").Value ?? string.Empty;
-
-        services.AddSingleton(typeof(IMongoClient), new MongoClient(dbConnString));
-        services.AddSingleton(typeof(IMongoDatabase), x =>
-        {
-            var client = x.GetRequiredService<IMongoClient>();
-            return client.GetDatabase("Db");
-        });
-
-        services.AddSingleton(typeof(IMongoCollection<Content>), x =>
-        {
-            var database = x.GetRequiredService<IMongoDatabase>();
-            return database.GetCollection<Content>("WebsiteContent");
-        });
-
-        services.AddSingleton(typeof(IMongoCollection<Weather>), x =>
-        {
-            var database = x.GetRequiredService<IMongoDatabase>();
-            return database.GetCollection<Weather>("Weather");
-        });
     }
 }
